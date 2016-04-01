74732d3865c1c7aadab16386c1d196be571fbe32
Taylor Pokoj <pokojt@gmail.com>
2016-03-31 20:52:00 -0700

    Add basic file structure and being coding first question route and tile

---
+++ app/components/new-question.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  actions: {
+    toggleDetail() {
+      this.toggleProperty('questionFormShowing');
+    },
+    saveQuestion() {
+      var formInputs = {
+        content: this.get('content') ? this.get('content'):"",
+        author: this.get('author') ? this.get('author'):"",
+        notes: this.get('notes') ? this.get('notes'):"",
+      };
+      this.toggleProperty('questionFormShowing');
+      this.set('name', "");
+      this.set('author',"");
+      this.set('notes',"");
+      this.sendAction('saveQuestion', formInputs);
+    }
+  }
+});
--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'ask-me',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://ask-me-taylor.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
