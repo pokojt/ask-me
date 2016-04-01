74957b897b01ee94b1ef4d4be3647c43e7f35e5f
Taylor Pokoj <pokojt@gmail.com>
2016-04-01 10:54:17 -0700

    Fixed bug on new-question.js after adding the answers

--- app/components/new-question.js
+++ app/components/new-question.js
@@ -10,7 +10,7 @@ export default Ember.Component.extend({
         content: this.get('content') ? this.get('content'):"",
         author: this.get('author') ? this.get('author'):"",
         notes: this.get('notes') ? this.get('notes'):"",
-        answers: this.get('answers'),
+        // answer: this.get('answer'),
       };
       this.toggleProperty('questionFormShowing');
       this.set('name', "");
